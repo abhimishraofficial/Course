1cda6d3244ec0ceb79b254ebc2bc77a702f19242
Abhishek Mishra <shashankmishrahjp@gmail.com>
2025-11-06 22:24:44 +0530

    Initial commit - added Course folder

---
+++ Demo.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        System.out.println("enter a number");
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // BufferedReader bf = new BufferedReader(in);
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+            System.out.println(num);
+           
+
+        }
+ 
+      }
+
