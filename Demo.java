3ed3a5f43dd476a950d013bbfbf691210f813ece
Abhishek Mishra <shashankmishrahjp@gmail.com>
2025-11-29 07:59:48 +0530

    Updated Demo.java logic

--- Demo.java
+++ Demo.java
@@ -1,19 +1,36 @@
-import java.io.IOException;
-import java.util.Scanner;
+class Counter {
+    int count;
 
-
+    public synchronized void increment() {
+        count++;
+    }
+}
 
 public class Demo {
-    public static void main(String[] args) throws IOException {
-        System.out.println("enter a number");
-        // InputStreamReader in = new InputStreamReader(System.in);
-        // BufferedReader bf = new BufferedReader(in);
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-            System.out.println(num);
-           
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
 
-        }
- 
-      }
+        Runnable obj1 = () -> {
+            for (int i = 1; i <= 10000; i++) {
+                c.increment();
+            }
+        };
 
+        Runnable obj2 = () -> {
+            for (int i = 1; i <= 10000; i++) {
+                c.increment();
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(c.count);  // always 20000
+    }
+}
